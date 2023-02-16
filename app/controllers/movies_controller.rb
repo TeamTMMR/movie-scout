0d4515b2247955eb5ee7995e9de7301c4131066c
mollykwolf <mollykwolf@gmail.com>
2023-02-16 14:41:19 -0800

    completed create endpoint (backend)

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,13 +5,23 @@ class MoviesController < ApplicationController
         render json: movies
     end
 
-    # def create
-    # end
+    def create
+        movie = Movie.create(movie_params)
+        if movie.valid? 
+            render json: movie
+        else 
+            render json: movie.errors, status:422
+        end
+    end
 
     # def update
     # end
 
     # def destroy
     # end
+    private 
+    def movie_params 
+        params.require(:movie).permit(:title, :release_year, :genre, :run_time, :description, :img_url, :user_id)
+    end 
 
 end
