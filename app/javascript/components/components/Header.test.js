e1cbe587e27032e1672dbff8381aee6a4eab8734
mollykwolf <mollykwolf@gmail.com>
2023-02-15 16:43:35 -0800

    completed navigation

--- app/javascript/components/components/Header.test.js
+++ app/javascript/components/components/Header.test.js
@@ -1,10 +1,21 @@
 import React from "react"
-import { render } from "@testing-library/react"
+import { render, screen} from "@testing-library/react"
 import Header from "./Header"
+import { BrowserRouter } from "react-router-dom"
 
 describe("<Header />", () => {
   it("renders without crashing", () => {
     const div = document.createElement("div")
     render(<Header />, div)
+  })
+  it("has a clickable links", () => {
+    render(
+      <BrowserRouter>
+          <Header />
+      </BrowserRouter>
+    )
+    screen.debug()
+    const text = screen.getByText(/all movies/i)
+    expect(text).toBeInTheDocument()
   })
 })
