150d75dd37607481f3512f58485f001375b0559d
Moss <mossc2001@gmail.com>
2023-02-17 16:13:08 -0800

    finished all trello card requirements for protected create frontend

--- app/javascript/components/pages/MovieNew.test.js
+++ app/javascript/components/pages/MovieNew.test.js
@@ -1,10 +1,20 @@
 import React from "react"
-import { render } from "@testing-library/react"
+import { render, screen } from "@testing-library/react"
 import MovieNew from "./MovieNew"
+import { BrowserRouter } from "react-router-dom"
 
 describe("<MovieNew />", () => {
   it("renders without crashing", () => {
     const div = document.createElement("div")
     render(<MovieNew />, div)
+  })
+  it("has a form description", () => {
+    render(
+      <BrowserRouter>
+        <MovieNew />
+      </BrowserRouter>
+    )
+    const upload = screen.getByText(/Add/i)
+    expect(upload).toBeInTheDocument()
   })
 })
