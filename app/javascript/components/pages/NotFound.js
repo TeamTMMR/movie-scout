cd455832a0712ef8d58f1d48d18239944d54e26d
TorinJacobs <potours@yahoo.com>
2023-02-22 14:00:43 -0800

    completed mvp cards

--- app/javascript/components/pages/NotFound.js
+++ app/javascript/components/pages/NotFound.js
@@ -1,9 +1,13 @@
 import React from "react"
+import notfoundpic from "../assets/404.png"
 
 const NotFound = () => {
   return (
     <>
       <h3>404 Error Page Not Found</h3>
+      <div className="error">
+        <img src={notfoundpic}/>
+      </div>
     </>
   )
 }
