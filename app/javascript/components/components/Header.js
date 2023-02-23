618c0b086a67053942ca52d5d42665c8687ea454
RaymondOchoa <ochoaraymondb@yahoo.com>
2023-02-23 11:13:19 -0800

    header styled

--- app/javascript/components/components/Header.js
+++ app/javascript/components/components/Header.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import {
   Collapse,
+  MBDContainer,
   Navbar,
   NavbarToggler,
   NavbarBrand,
@@ -13,58 +14,57 @@ import {
 const Header = ({sign_in_route, logged_in, current_user, new_user_route,sign_out_route}) => {
   return (
   <>
-    <div>
+    <div className='header'>
       <Navbar>
         <NavbarBrand href="/">Movie Scout</NavbarBrand>
-  
           <Nav className="me-auto" navbar>
         {!logged_in && (
-          <NavItem>
-            <NavLink>
-              <a href="/movieindex" className="nav-link">
-                All Movies
-              </a>
-            </NavLink>
-            <NavLink>
-              <a href={sign_in_route} className="nav-link">
-                Sign In
-              </a>
-            </NavLink>
-            <NavLink>
-              <a href={new_user_route} className="nav-link">
-                Sign Up
-              </a>
-            </NavLink>
-          </NavItem>
+            <NavItem className='nav-item'>
+              <NavLink>
+                <a href="/movieindex" className="nav-link">
+                  All Movies
+                </a>
+              </NavLink>
+              <NavLink>
+                <a href={sign_in_route} className="nav-link">
+                  Sign In
+                </a>
+              </NavLink>
+              <NavLink>
+                <a href={new_user_route} className="nav-link">
+                  Sign Up
+                </a>
+              </NavLink>
+            </NavItem>
         )}
         {logged_in && (
-          <NavItem>
-            <NavLink>
-              <a href="/movieindex" className="nav-link">
-                All Movies
-              </a>
-            </NavLink>
-            <NavLink>
-              <a href="/mymovies" className="nav-link">
-                My Movies
-              </a>
-            </NavLink>
-            <NavLink>
-              <a href={sign_out_route} className="nav-link">
-                Sign Out
-              </a>
-            </NavLink>
-            <NavLink>
-              <a href= "/aboutus" className="nav-link">
-                About Us
-              </a>
-            </NavLink>
-            <NavLink>
-              <a href="/movienew" className="nav-link">
-                Upload Movie
-              </a>
-            </NavLink>
-          </NavItem>
+            <NavItem>
+              <NavLink>
+                <a href="/movieindex" className="nav-link">
+                  All Movies
+                </a>
+              </NavLink>
+              <NavLink>
+                <a href="/mymovies" className="nav-link">
+                  My Movies
+                </a>
+              </NavLink>
+              <NavLink>
+                <a href={sign_out_route} className="nav-link">
+                  Sign Out
+                </a>
+              </NavLink>
+              <NavLink>
+                <a href= "/aboutus" className="nav-link">
+                  About Us
+                </a>
+              </NavLink>
+              <NavLink>
+                <a href="/movienew" className="nav-link">
+                  Upload Movie
+                </a>
+              </NavLink>
+            </NavItem>
         )}
           </Nav>
       </Navbar>
